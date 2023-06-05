d6c6a9e9c4b447a895684fedaf3ebbd8f45be470
Rodevp <rodriguezrodrigor12@gmail.com>
2023-06-05 11:44:08 -0500

    add flag

--- dollar/src/app/components/carddollar/carddollar.component.ts
+++ dollar/src/app/components/carddollar/carddollar.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carddollar',
   templateUrl: './carddollar.component.html',
-  styleUrls: ['./carddollar.component.css']
 })
-export class CarddollarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CarddollarComponent{}
